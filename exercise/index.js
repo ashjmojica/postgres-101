bc85f6195cf7c3c6260aaf09fb95abe45eb1c5f1
Ashley Mojica <ashjmojica@gmail.com>
2023-05-23 12:03:51 -0500

    Update index.js

--- exercise/index.js
+++ exercise/index.js
@@ -1,1 +1,22 @@
 require("dotenv").config();
+const express = require("express");
+const app = express();
+const { pool } = require("pg");
+const pool = new pool({
+    host: "localhost",
+    port: 5432,
+    database: "resturant",
+    user: "postgres",
+    password: process.env.DATABASE_PASSWORD,
+});
+app.use(express.json());
+//orders
+app.get("/get_orders", (req, res) => {
+    pool.connect((err, client, release) => {
+        if (err) {
+            release();
+console.error("error connecting to the database");
+
+        }
+    })
+}
